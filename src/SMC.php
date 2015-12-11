c530826e5a69075686260dc4a2ac1f1bf38a3538
Sam Schmidt <samuel@dersam.net>
2015-12-11 14:55:58 -0500

    Parse source files directly, as reflecting for the filename is not reliable if already smc'd

--- src/SMC.php
+++ src/SMC.php
@@ -9,6 +9,13 @@
  */
 class SMC
 {
+    private static $modifiedMethods = array();
+
+    public static function getModifiedMethods()
+    {
+        return self::$modifiedMethods;
+    }
+
     public static function reload_class($class)
     {
         $ref = new ReflectionClass($class);
@@ -18,10 +25,38 @@ class SMC
         return runkit_import($filename, RUNKIT_IMPORT_OVERRIDE|RUNKIT_IMPORT_CLASS_METHODS);
     }
 
-    public static function reload_method($classname, $methodname, $args = array())
+    /**
+     * Runkit changes the source file of a method, so we have to track methods
+     * once we modify them, otherwise SMC crashes after first reload.
+     * @param $classname
+     * @param $methodname
+     * @param $filename
+     */
+    public static function trackModifiedMethod($classname, $methodname, $filename)
     {
-        $args = implode(',', $args);
+        if (!isset(self::$modifiedMethods[$classname])) {
+            self::$modifiedMethods[$classname] = array();
+        }
 
+        if (!isset(self::$modifiedMethods[$classname][$methodname])) {
+            self::$modifiedMethods[$classname][$methodname] = array();
+        }
+
+        self::$modifiedMethods[$classname][$methodname] = $filename;
+    }
+
+    public static function getTrackedMethodFilename($classname, $methodname)
+    {
+        return
+        isset(self::$modifiedMethods[$classname])
+        && isset(self::$modifiedMethods[$classname][$methodname])
+        ?
+            self::$modifiedMethods[$classname][$methodname]
+            : false;
+    }
+
+    public static function reload_method($classname, $methodname)
+    {
         $method = new ReflectionMethod($classname, $methodname);
         $visibility = RUNKIT_ACC_PUBLIC;
 
@@ -38,21 +73,66 @@ class SMC
         return runkit_method_redefine(
             $classname,
             $methodname,
-            $args,
+            self::getMethodArguments($classname, $methodname),
             self::getMethodCode($classname, $methodname),
             $visibility
         );
     }
 
+    public static function getMethodArguments($class, $method)
+    {
+        $method = new ReflectionMethod($class, $method);
+        $methodname = $method->getName();
+        $classname = $method->getDeclaringClass()->getName();
+        if (! $filename =
+            self::getTrackedMethodFilename($classname, $methodname)) {
+            $filename = $method->getFileName();
+            self::trackModifiedMethod($classname, $methodname, $filename);
+        }
+
+        //Ensure that the method signature is on one line
+        $source = file_get_contents($filename);
+        $source = str_replace("\n", "", $source);
+        $source = str_replace("\r", "", $source);
+
+        //Get the args
+        $regex = '/function '.$methodname.'\(([\s\S]*?)\)/';
+        $args = array();
+        preg_match($regex, $source, $args);
+
+        if (empty($args)) {
+            $args = '';
+        } else {
+            $args = str_replace(' ', '',$args[1]);
+        }
+
+        return $args;
+    }
+
     public static function getMethodCode($class, $method)
     {
         $method = new ReflectionMethod($class, $method);
-        $filename = $method->getFileName();
-        $start_line = $method->getStartLine() + 1;
-        $end_line = $method->getEndLine()-1;
-        $length = $end_line - $start_line;
+        $methodname = $method->getName();
+        $classname = $method->getDeclaringClass()->getName();
+        if (! $filename =
+            self::getTrackedMethodFilename($classname, $methodname)) {
+            $filename = $method->getFileName();
+            self::trackModifiedMethod($classname, $methodname, $filename);
+        }
 
-        $source = file($filename);
-        return implode("", array_slice($source, $start_line, $length));
+        $source = file_get_contents($filename);
+
+        //This regex will find the method body
+        $regex = '/function '.$methodname.'*?[\s\S]*?\{([\s\S]*?)\}/';
+        $body = array();
+        preg_match($regex, $source, $body);
+
+        if (empty($body)) {
+            $body = '';
+        } else {
+            $body = $body[1];
+        }
+
+        return $body;
     }
 }
